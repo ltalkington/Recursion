1ccecfecf886632c1151e2fb056a2363f1d9ea00
Logan Talkington <talkingl@oregonstate.edu>
2022-09-10 15:54:42 -0500

    head iteration and head recursions

--- main.cpp
+++ main.cpp
@@ -16,9 +16,31 @@ void tailIteration(int n)
     }
 }
 
+void headRecursion(int n)
+{
+    if(n>0) {
+        headRecursion(n-1);
+        printf("%d", n);
+    }
+}
+
+void headIteration(int n) {
+    int i = 1;
+    while(i <=n) {
+        printf("%d", i);
+        i++;
+    }
+}
+
 int main() {
     int x = 3;
+    printf("Tail Recursion:");
     tailRecursion(x);
+    printf("\nTail Iteration:");
     tailIteration(x);
+    printf("\nHead Recursion:");
+    headRecursion(x);
+    printf("\nHead Iteration:");
+    headIteration(x);
     return 0;
 }

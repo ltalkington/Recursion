82d4d91918046e77ac11e1e457416eac8a520e29
Logan Talkington <talkingl@oregonstate.edu>
2022-09-10 15:25:19 -0500

    test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int func(int n)
+{
+    static int x=0;
+    if(n>0){
+        x++;
+        return func(n-1 ) + x ;
+    }
+    return 0;
+}
+
+int main() {
+    int x = 3;
+
+    printf("%d", func(x));
+
+    return 0;
+}
